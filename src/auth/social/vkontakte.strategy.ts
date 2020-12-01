8d157aaa04ac369b5c7029ac306e0915c2cd45d9
Ilham <ilham.pirm@gmail.com>
2020-12-02 01:20:11 +0600

    Fixed VK auth

--- src/auth/social/vkontakte.strategy.ts
+++ src/auth/social/vkontakte.strategy.ts
@@ -1,23 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Profile, Strategy,VerifyCallback } from "passport-vkontakte";
+import {Injectable} from "@nestjs/common";
+import {PassportStrategy} from "@nestjs/passport";
+import {Strategy, VerifyCallback} from "passport-vkontakte";
 import {UserService} from "../../user/user.service";
 
 @Injectable()
 export class VkontakteStrategy extends PassportStrategy(Strategy, "vkontakte") {
     constructor(private readonly userService: UserService) {
         super({
+            passReqToCallBack: false,
             clientID: '7678205',
             clientSecret: 'kaEWZCSsu8CyyQnnUAXG',
             callbackURL: "https://photo-backend-app.herokuapp.com/api/auth/vkontakte/redirect",
-            scope: ['id', 'email', 'displayName', 'name','gender']
-            // profileFields: ['id', 'displayName', 'name', 'gender','email', 'picture.type(large)']
+            scope: ['id', 'displayName', 'name', 'gender', 'email', 'photos'],
+            profileFields: ["email"]
+        }, async (accessToken: any,
+                  refreshToken: any,
+                  params: any,
+                  profile: any,
+                  done: VerifyCallback) => {
+            console.log(profile);
+            const user = await this.userService.findOrCreate(profile)
+            done(null, user)
         });
-    }
-    async validate(accessToken:any, refreshToken:any, params:any, profile: any, done:any): Promise<any> {
-        const user = await this.userService.findOrCreate(
-            profile,
-        );
-        done(null, user);
     }
 }
