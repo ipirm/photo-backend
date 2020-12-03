58e6d8b8f041bad4f8d1aeb0551c6c8b205f3e6a
Ilham <ilham.pirm@gmail.com>
2020-12-03 18:10:12 +0600

    Heroku work please

--- src/main.ts
+++ src/main.ts
@@ -23,12 +23,11 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
     app.enableCors();
 
-    console.log(process.env.AWS_REGION)
-    config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION
-    });
+    // config.update({
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //     region: process.env.AWS_REGION
+    // });
     // app.useWebSocketAdapter(new WsAdapter());
     await app.listen(process.env.PORT || 3000);
 }
