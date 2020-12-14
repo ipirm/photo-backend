8291a8f4ec54ad353a7261a8cdf8807c82fbe948
Ilham <ilham.pirm@gmail.com>
2020-12-14 18:31:35 +0400

    Soft DELETE

--- src/helpers/change-file-path.ts
+++
@@ -1,4 +0,0 @@
-
-export const editFilePath = (req, file, callback) => {
-    callback(null, `${process.env.FILE_URL}${file.filename}`);
-}
--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -4,12 +4,11 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {LikesEntity} from "../entities/likes.entity";
 import {LikeService} from "./like.service";
 import {ConcertsUsersEntity} from "../entities/concerts-users.entity";
-import {LikeGateway} from "./like.gateway";
 
 @Module({
 
     imports: [TypeOrmModule.forFeature([LikesEntity,ConcertsUsersEntity])],
-    providers: [LikeService,LikeGateway],
+    providers: [LikeService],
     controllers: [LikeController]
 })
 export class LikeModule {
