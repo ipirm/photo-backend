462aaa264bda8b76787fd31463b4b0584dd3c047
Ilham <ilham.pirm@gmail.com>
2020-12-27 04:43:08 +0400

    Fixed Bg

--- src/paypal/paypal.controller.ts
+++ src/paypal/paypal.controller.ts
@@ -1,7 +1,7 @@
 import {Controller, Get, Query, Res, UseGuards} from '@nestjs/common';
 import {ApiTags} from "@nestjs/swagger";
 
-const paypal = require('paypal-rest-sdk');
+const paypal = require('@paypal/checkout-server-sdk');
 import {PaypalService} from "./paypal.service";
 import {JwtAuthGuard} from "../auth/jwt/jwt-auth.guard";
 import {User} from "../decorators/user.decorator";
@@ -16,46 +16,44 @@ export class PaypalController {
 
     // @UseGuards(JwtAuthGuard)
     @Get('/')
-    async getPay(@Res() res, @User() user: any): Promise<Boolean> {
-        console.log(process.env.PAYPAL_CLIENT_ID)
-        console.log(process.env.PAYPAL_CLIENT_SECRET)
-        const create_payment_json = {
-            "intent": "sale",
-            "payer": {
-                "payment_method": "paypal"
-            },
-            "redirect_urls": {
-                "return_url": `https://photo-backend-app.herokuapp.com/api/paypal/success?user_id=17`,
-                "cancel_url": "https://photo-backend-app.herokuapp.com/api/paypal/cancel"
-            },
-            "transactions": [{
-                "item_list": {
-                    "items": [{
-                        "name": "item",
-                        "sku": "item",
-                        "price": "1.00",
-                        "currency": "USD",
-                        "quantity": 1
-                    }]
-                },
-                "amount": {
-                    "currency": "USD",
-                    "total": "1.00"
-                },
-                "description": "Participation in Contest"
-            }]
-        };
-        await paypal.payment.create(create_payment_json, (error, payment) => {
-            if (error)
-                throw error
+    async getPay(@Res() res, @User() user: any): Promise<void> {
 
-            for (let i = 0; i < payment.links.length; i++) {
-                if (payment.links[i].rel === 'approval_url') {
-                    return res.redirect(payment.links[i].href)
+// Creating an environment
+        let clientId = process.env.PAYPAL_CLIENT_ID;
+        let clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+// This sample uses SandboxEnvironment. In production, use LiveEnvironment
+        let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+        let client = new paypal.core.PayPalHttpClient(environment);
+
+        let request = new paypal.orders.OrdersCreateRequest();
+        request.requestBody({
+            "intent": "CAPTURE",
+            "purchase_units": [
+                {
+                    "amount": {
+                        "currency_code": "USD",
+                        "value": "100.00"
+                    }
+                }
+            ]
+        });
+
+// Call API with your client and get a response for your call
+        let createOrder  = async function(){
+            let response = await client.execute(request);
+            // console.log(`Response: ${JSON.stringify(response)}`);
+            // If call returns body in response, you can get the deserialized version from the result attribute of the response.
+            // console.log(`Order: ${JSON.stringify(response.result)}`);
+            for (let i = 0; i < response.result.links.length; i++) {
+                if (response.result.links[i].rel === 'approve') {
+                    console.log(response.result.links[i].rel)
+                    return res.redirect(response.result.links[i].href)
                 }
             }
-        });
-        return true
+            }
+        createOrder();
+
+        // return true
     }
 
     @Get('/success')
