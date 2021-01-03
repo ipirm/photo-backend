940cb63544332921822a05bb2ded30bfd19d15d3
Ilham <ilham.pirm@gmail.com>
2021-01-03 18:53:53 +0400

    Fixed App

--- src/main.ts
+++ src/main.ts
@@ -11,26 +11,26 @@ const paypal = require('paypal-rest-sdk');
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.setGlobalPrefix('api');
-    app.useGlobalPipes(new ValidationPipe());
+    await app.setGlobalPrefix('api');
+    await app.useGlobalPipes(new ValidationPipe());
     const options = new DocumentBuilder()
         .setDescription('The  API description')
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, options);
-    config.update({
+    await config.update({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         region: process.env.AWS_REGION
     });
-    paypal.configure({
+    await paypal.configure({
         'mode': 'sandbox', //sandbox or live
         'client_id': process.env.PAYPAL_CLIENT_ID,
         'client_secret': process.env.PAYPAL_CLIENT_SECRET
     });
-    SwaggerModule.setup('api', app, document);
-    app.enableCors();
-    app.use(helmet());
+    await SwaggerModule.setup('api', app, document);
+    await app.enableCors();
+    await app.use(helmet());
     await app.listen(process.env.PORT || 8080);
 }
 
