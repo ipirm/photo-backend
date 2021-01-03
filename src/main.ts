938f4ee0589b1a473cf24c95908779a72b4fe344
Ilham <ilham.pirm@gmail.com>
2021-01-03 19:03:48 +0400

    Fixed App

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,26 @@ import {ValidationPipe} from "@nestjs/common";
 import * as helmet from 'helmet';
 import {config} from 'aws-sdk';
 
-const paypal = require('paypal-rest-sdk');
-
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    await app.setGlobalPrefix('api');
-    await app.useGlobalPipes(new ValidationPipe());
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe());
     const options = new DocumentBuilder()
         .setDescription('The  API description')
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, options);
-    await config.update({
+    config.update({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         region: process.env.AWS_REGION
     });
-    await paypal.configure({
-        'mode': 'sandbox', //sandbox or live
-        'client_id': process.env.PAYPAL_CLIENT_ID,
-        'client_secret': process.env.PAYPAL_CLIENT_SECRET
-    });
-    await SwaggerModule.setup('api', app, document);
-    await app.enableCors();
-    await app.use(helmet());
-    await app.listen(process.env.PORT || 8080);
+    SwaggerModule.setup('api', app, document);
+    app.enableCors();
+    app.use(helmet());
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
