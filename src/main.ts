657e03cae16fe086f8a4c9bd151fee1ad30336c4
Ilham <ilham.pirm@gmail.com>
2021-01-04 15:38:15 +0400

    Fixed Prefomance

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
 import * as helmet from 'helmet';
 import {config} from 'aws-sdk';
-
+import * as compression from 'compression';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -24,6 +24,7 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
     app.enableCors();
     app.use(helmet());
+    app.use(compression());
     await app.listen(process.env.PORT || 3000);
 }
 
